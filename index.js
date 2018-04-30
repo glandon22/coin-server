f1c0f0c3bb569d48c9ad4138222a1d006d5bc2a6
glandon22 <glandonwright@gmail.com>
2018-04-30 05:42:48 -0400

    setting up

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+var moment = require('moment');
+moment().format();
+const app = express();
+const PORT = process.env.PORT || 5000;
+var yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+var labels = [];
+const mysql = require('mysql');
+
+app.get('/', function(req,res) {
+    res.send('hello world');
+})
