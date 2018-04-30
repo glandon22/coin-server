63358ce197dc9bd309068ad93839323f6d1d7f70
glandon22 <glandonwright@gmail.com>
2018-04-30 08:07:50 -0400

    scraping in data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 var moment = require('moment');
 moment().format();
 const schedule = require('node-schedule');
+const _async = require('async');
 const app = express();
 const PORT = process.env.PORT || 5000;
 var yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
@@ -12,6 +13,7 @@ var request = require('request');
 var coins = [
     'btc', 'bch', 'ltc', 'eth', 'xem', 'zec', 'dash', 'xmr', 'xvg', 'btg'
 ];
+var newData = [];
 
 //wrap function in this after i get it working
 /*
@@ -21,8 +23,15 @@ schedule.scheduleJob('0 0 * * *', () => {
 */
 
 coins.forEach(coin => {
-    request.get('https://coinmetrics.io/data/' + coin + '.csv', function(req,res) {
-        console.log(coin);
+    request.get('https://coinmetrics.io/data/' + coin + '.csv', function(err, res, body) {
+        var newRow = [];
+        var csv = body.split(',');
+        for (var i = 0; i < 9; i++) {
+            newRow.unshift(csv[csv.length - 2 - i]);
+        }
+        newRow.unshift(coin);
+        console.log(newRow);
+
     });
 });
 
