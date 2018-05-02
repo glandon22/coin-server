99da03bed0da9ffbf547b7dec63cae6a33a70685
glandon22 <glandonwright@gmail.com>
2018-05-02 07:42:03 -0400

    finished node schedule task

--- index.js
+++ index.js
@@ -17,28 +17,26 @@ var newData = [];
 
 //wrap function in this after i get it working
 
-schedule.scheduleJob('37 * * * *', () => { 
+schedule.scheduleJob('5 4 * * *', () => { 
     coins.forEach(coin => {
         request.get('https://min-api.cryptocompare.com/data/price?fsym=' +  coin.toUpperCase() + '&tsyms=USD', function(err, res, body) {
             var coinPrice = JSON.parse(body);
             newData.push([coin, moment().format('YYYY-MM-DD'), coinPrice.USD]);
             if (newData.length === 10) {
                 var con = mysql.createConnection({
-                    host: "cryptos.cvndjrqk9gtt.us-east-2.rds.amazonaws.com",
+                    host: process.env.HOST,
                     user: process.env.USERNAME,
                     password: process.env.PASSWORD,
-                    database: "cryptos"
+                    database: process.env.DATABASE
                   });
                   
                   con.connect(function(err) {
                     if (err) throw err;
-                    console.log(err);
                     console.log("Connected!");
             
-                    var sql = "INSERT INTO test (coin, date, price) VALUES ?";
+                    var sql = "INSERT INTO coins (coin, date, price) VALUES ?";
                     con.query(sql, [newData], function(err, results) {
                         if (err) throw err;
-                        console.log(err);
                         console.log('inserted correctly');
                         newData = [];
                     }); 
